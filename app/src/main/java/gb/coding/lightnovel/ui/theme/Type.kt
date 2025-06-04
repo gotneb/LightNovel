36b9ffcf58c72885d889bce7f173b08705a55701
Gabriel Bento <gabrielorigenstdb@gmail.com>
2025-06-03 23:00:23 -0300

    chore: add `SourceSerif4` font

--- app/src/main/java/gb/coding/lightnovel/ui/theme/Type.kt
+++ app/src/main/java/gb/coding/lightnovel/ui/theme/Type.kt
@@ -2,9 +2,19 @@ package gb.coding.lightnovel.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import gb.coding.lightnovel.R
+
+val SourceSerif4 = FontFamily(
+    Font(R.font.source_serif_4_light, FontWeight.Light),
+    Font(R.font.source_serif_4, FontWeight.Normal),
+    Font(R.font.source_serif_4_medium, FontWeight.Medium),
+    Font(R.font.source_serif_4_semibold, FontWeight.SemiBold),
+    Font(R.font.source_serif_4_bold, FontWeight.Bold),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
