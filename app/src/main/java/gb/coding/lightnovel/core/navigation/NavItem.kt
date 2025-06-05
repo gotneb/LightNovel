81f171925c016133f45b489e4b934b24abb6496f
Gabriel Bento <gabrielorigenstdb@gmail.com>
2025-06-04 22:03:10 -0300

    feat: add filled icon on bottom bar when selected

--- app/src/main/java/gb/coding/lightnovel/core/navigation/NavItem.kt
+++ app/src/main/java/gb/coding/lightnovel/core/navigation/NavItem.kt
@@ -1,14 +1,11 @@
 package gb.coding.lightnovel.core.navigation
 
 import androidx.annotation.DrawableRes
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.painterResource
 import gb.coding.lightnovel.R
 
 data class NavItem(
     val label: String,
+    @DrawableRes val iconFilledResId: Int,
     @DrawableRes val iconResId: Int,
     val route: Route
 )
@@ -16,12 +13,14 @@ data class NavItem(
 val bottomNavItems = listOf(
     NavItem(
         label = "Biblioteca",
+        iconFilledResId = R.drawable.book_filled,
         iconResId = R.drawable.book,
-        route = Route.Library
+        route = Route.Library,
     ),
     NavItem(
         label = "Pesquisar",
+        iconFilledResId = R.drawable.explore_filled,
         iconResId = R.drawable.explore,
-        route = Route.Browse
+        route = Route.Browse,
     ),
 )
--- app/src/main/java/gb/coding/lightnovel/core/navigation/components/BottomNavigationBar.kt
+++ app/src/main/java/gb/coding/lightnovel/core/navigation/components/BottomNavigationBar.kt
@@ -5,7 +5,6 @@ import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import gb.coding.lightnovel.core.navigation.NavItem
 
@@ -17,13 +16,14 @@ fun BottomNavigationBar(
 ) {
     NavigationBar {
         items.forEachIndexed { index, item ->
+            val selected = selectedNavigationIndex == index
             NavigationBarItem(
-                selected = selectedNavigationIndex == index,
+                selected = selected,
                 label = { Text(item.label) },
                 onClick = { onItemSelected(index) },
                 icon = {
                     Icon(
-                        painter = painterResource(id = item.iconResId),
+                        painter = painterResource(id = if (selected) item.iconFilledResId else item.iconResId),
                         contentDescription = item.label,
                     )
                 },
